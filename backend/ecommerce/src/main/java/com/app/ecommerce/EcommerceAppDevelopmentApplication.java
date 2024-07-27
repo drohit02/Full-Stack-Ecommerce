ed374ca241be506851ce32d4109d9476df747fc2
drohit02 <drohit045@gmail.com>
2024-07-27 21:56:18 +0530

    Backend for Ecommerce is created using SpringBoot Initial state

---
+++ backend/ecommerce/src/main/java/com/app/ecommerce/EcommerceAppDevelopmentApplication.java
@@ -0,0 +1,13 @@
+package com.app.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceAppDevelopmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceAppDevelopmentApplication.class, args);
+	}
+
+}
